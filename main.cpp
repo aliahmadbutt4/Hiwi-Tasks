28966eca6a5503f1dcb6cc8b267820c10479d54d
aliahmadbutt4 <aliahmadbutt6@gmail.com>
2020-08-13 01:22:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include <tuple>
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
 
-using namespace std;
 
 int main() {
-	cout<<"Hello World!"<<endl;
+	std::cout<<"Hello World!"<<std::endl;
     return 0;
 }
